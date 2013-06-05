497f7d44bcc18209da9a4b19ea648a2ab1e81430
Amy Hua <foramyhua@gmail.com>
2013-06-05 07:06:28 -0300

    Update gameboard.rb
    Began AI Component

--- gameboard.rb
+++ gameboard.rb
@@ -18,9 +18,24 @@
     attr_accessor :board
     attr_accessor :rboard #rotated board (rotated board by 90 degrees cw)
     attr_accessor :size
-    
+    attr_accessor :ai  # True = versus AI, False = versus human)
+    attr_accessor :turns_1 # no. turns of player 1
+    attr_accessor :turns_2 # no. turns of player 2
+    attr_accessor :u_1 # max payout of player 1 if player 1 plays the best next move possible
+    attr_accessor :u_2 # max payout of player 2 if player 2 plays the best next move possible
+
+    # payout for player i (i=1,2) is defined as:
+    #   u_i = { 10^23 - n  if can win in n turns
+    #           0          if tie
+    #          -10^23 + n  if will lose in n turns }
+
+    # The AI will always try to minimize the opponent's payout function,
+    #  i.e., win in the least number of turns, or if it can't win, tie
+    #  (or if it can't tie, lose in the longest number of turns)
   
     def initialize()
+      @turns_1 = 0
+      @turns_2 = 0
       puts ""
       puts "**********************************************"
       puts "**** WELCOME TO A NEW GAME OF TIC TAC TOE ****"
@@ -37,12 +52,14 @@
       size_s = gets.chomp
 
       @size = size_s.to_i
-      puts "***** LET'S BEGIN!  **************************"
+      
+      @ai = true #assume AI by default
+      self.determine_ai
+      
+      puts "*** LET'S BEGIN!  *****************************"
       
       @board = []
-      size.times do
-        self.board.push([])
-      end
+      size.times {self.board.push([])}
 
       for row in self.board
         size.times do
@@ -51,16 +68,39 @@
       end
       
       @rboard = []
-      size.times do
-        self.rboard.push([])
-      end
+      size.times {self.rboard.push([])}
+      
       for row in self.rboard
         size.times do
           row.push("_")
         end
       end
-
+      
     end
+    
+    def determine_ai()
+      puts "Do you want to play against a computer opponent? (Y/N): "
+      ai = gets.chomp
+      if ai.upcase == "Y"
+        puts "Human vs. AI"
+        ai = true
+      elsif ai.upcase == "N"
+        puts "Human vs. Human"
+        ai = false
+      else 
+        puts "*** ERROR: You must answer Y or N only. Try again ***"
+        self.determine_ai
+      end
+    end
+    
+    def add_turn(player)
+      if player == 1
+        @turns_1 = @turns_1.to_i.next
+      else
+        @turns_2 = @turns_2.to_i.next
+      end
+    end
+    
     # ------------------ ** PUBLISH GAMEBOARD ** -----------------
     # Updated: added row and column headers for easy (row, column) detection
     def print_board(option={})
@@ -96,14 +136,14 @@
           print "\n\n"
       end
     end
-        
+           
+    
+    # ------------ ** VICTORY CONDITIONS ** -------------
+
     def game_over?()
       self.board.flatten.all? {|entry| (entry != "_")}
     end
     
-    
-    # ------------ ** VICTORY CONDITIONS ** -------------
-
     def straight_win?(marker,m=size,option={}) # option: nil (original board) or 1 (rotated board)
       
       if option == {}
@@ -177,26 +217,54 @@
     
     # ------------ ** PLAYER ACTIONS ** -------------    
     
+    
     def move(player) # player = 1 or 2
       
     # update rotated board
 
-    for i in (0...size)
-      for j in (0...size)
-        self.rboard[i][j] = self.board[size-1-j][i]
+      for i in (0...size)
+        for j in (0...size)
+          self.rboard[i][j] = self.board[size-1-j][i]
+        end
       end
-    end
     
     # check if partial game
-     if won?(1)
-       puts "*** Player 1 WINS! ***"
-     elsif won?(2)
-       puts "*** PLAYER 2 WINS! ***"
-     elsif game_over?
-       puts "*** TIE! ***"
-     else
-       # is a partial game
-       # identify opponent and player
+      if won?(1)
+        puts "*** Player 1 WINS IN #{turns_1} TURNS! ***"
+      elsif won?(2)
+        puts "*** PLAYER 2 WINS IN #{turns_2} TURNS! ***"
+      elsif game_over?
+        puts "*** TIE! ***"
+      else
+        # is a partial game
+        # identify opponent and player
+        if player == 1
+          marker = "X" 
+          opponent = 2
+        end
+        if player == 2
+          marker = "O"
+          opponent = 1
+        end
+       
+        # prompt player to make their move
+       
+        print "Player " + player.to_s + ", make your move (row, column). Say EXIT to surrender: "
+       
+        move = gets.chomp # expected in "(i,j)" format
+      
+        if move.upcase == "EXIT"
+          puts "*** Player " + opponent.to_s + " WINS BY DEFAULT! ***"
+        else
+          mark_board(player,move)
+        end
+      end
+    end
+      
+      
+    def mark_board(player, move)
+      
+      # determine mark and opponent
       if player == 1
         marker = "X" 
         opponent = 2
@@ -205,9 +273,7 @@
         marker = "O"
         opponent = 1
       end
-      # prompt player to make their move
-      print "Player " + player.to_s + ", make your move (row, column). Say EXIT to surrender: "
-      move = gets.chomp # expected in "(i,j)" format
+      
       # error-check for proper format
       if move =~ /[(](\d|\d\d)|[,](\d|\d\d)[)]/
         i = move.match(/(\d\d|\d)/).to_s.to_i
@@ -222,6 +288,7 @@
             puts ""
             print_board
             puts ""
+            add_turn(player)
             move(opponent)
           else
             puts "***** ERROR: That position is already taken ********"
@@ -233,17 +300,11 @@
           puts "***** Try again                                  *********"
           move(player)
         end
-      elsif move.upcase != "EXIT"
+      else
         puts "***** ERROR: improper format. Use (i,j) format     *********"
         puts "***** Try again                                    *********"
-        move(player)        
-      elsif move.upcase == "EXIT"
-        puts "*** Player " + opponent.to_s + " WINS! ***"
-      else
-        raise ArgumentError
+        move(player) 
       end
-     end
-      
     end
     
     # ------------ ** GAME ACTIONS ** -------------    
@@ -251,7 +312,28 @@
     
   end #class end
 
-# BEGIN GAME
+
+class TicTacToeAI < TicTacToe
+  # used in Human vs. AI games where AI must calculate potential outcomes
+  # during the AI's turn (who is always player 2)
+  
+  attr_accessor :game
+  attr_accessor :u1 #hash of possible moves => maximum payout for player 1
+  attr_accessor :u2 #hash of possible moves => maximum payout for player 2
+  attr_accessor :best_next_move
+  attr_accessor :left # number of total possible moves on the gameboard
+  
+  def initialize(gameboard)
+    @game = gameboard
+  end
+  
+  def get_u1(game)
+    
+    
+  end
+end
+
+# ---------------------- BEGIN GAME ----------------------------
 
 def new_game()
   g = TicTacToe.new
